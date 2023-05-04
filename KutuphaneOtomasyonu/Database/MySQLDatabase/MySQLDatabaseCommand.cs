549b6d63a6a712467a0fe86bd6e1fadf6fc724ef
SEFATAS1 <93452541+SEFATAS1@users.noreply.github.com>
2023-05-04 13:24:30 +0300

    Database operations
    Connection to database and command methods for database have been created.

--- KutuphaneOtomasyonu/Database/MySQLDatabase/MySQLDatabaseCommand.cs
+++ KutuphaneOtomasyonu/Database/MySQLDatabase/MySQLDatabaseCommand.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows.Controls;
 
 namespace Database.MySQLDatabase
 {
-    internal class MySQLDatabaseCommand
+    public class MySQLDatabaseCommand
     {
+        MySQLDatabaseConnection MyConnection = new MySQLDatabaseConnection();
+
+        public MySqlCommand MyCommand(string SqlQuery)
+        {
+            MySqlCommand cmd = new MySqlCommand(SqlQuery, MyConnection.ConnectionOpen());
+            return cmd;
+        }
+
+        public bool MyReader(MySqlCommand cmd)
+        {
+            MySqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                dr.Close();
+                MyConnection.ConnectionClose();
+                return true;
+            }
+            else
+            {
+                dr.Close();
+                MyConnection.ConnectionClose();
+                return false;
+            }
+        }
+
+        public MySqlDataAdapter MySelectCommand(string SqlQuery, DataGrid dataGrid)
+        {
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new(SqlQuery, MyConnection.ConnectionOpen());
+            da.Fill(dt);
+            dataGrid.ItemsSource = dt.DefaultView;
+            return da;
+        }
     }
 }
--- KutuphaneOtomasyonu/Database/MySQLDatabase/MySQLDatabaseConnection.cs
+++ KutuphaneOtomasyonu/Database/MySQLDatabase/MySQLDatabaseConnection.cs
@@ -1,6 +1,24 @@
-﻿namespace Database.MySQLDatabase
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Database.MySQLDatabase
 {
     internal class MySQLDatabaseConnection
     {
+        MySqlConnection cn = new MySqlConnection("SERVER=127.0.0.1;DATABASE=Kutuphane;UID=root;PWD=1234");
+
+        public MySqlConnection ConnectionOpen()
+        {
+            if (cn.State != ConnectionState.Open)
+                cn.Open();
+            return cn;
+        }
+
+        public MySqlConnection ConnectionClose()
+        {
+            if (cn.State != ConnectionState.Closed)
+                cn.Close();
+            return cn;
+        }
     }
 }
